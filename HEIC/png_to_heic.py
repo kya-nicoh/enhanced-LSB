0195205a86828a366578e74cbbe459d66f4b36f6
kya-nicoh <gabbnicoh@gmail.com>
2023-11-26 13:03:19 +0800

    added heic converter

--- HEIC/png_to_heic.py
+++ HEIC/png_to_heic.py
@@ -1,14 +1,22 @@
 from PIL import Image
+import pillow_heif
+import numpy as np
 
-def convert_png_to_heic(input_path, output_path):
-    # Open the PNG image
-    image = Image.open(input_path)
+pillow_heif.register_heif_opener()
 
-    # Save the image in HEIC format
-    image.save(output_path, format="HEIC")
+im = Image.open("HEIC/image.heic")  # do whatever need with a Pillow image
+im = im.rotate(13)
+im.save(f"rotated_image.heic", quality=90)
 
-# Example usage
-input_png_path = "HEIC\HEIC-Profilepic.heic"
-output_heic_path = "HEIC\HEIC-Profilepic.heic"
 
-convert_png_to_heic(input_png_path, output_heic_path)
+# if pillow_heif.is_supported("HEIC/image.heic"):
+#     heif_file = pillow_heif.open_heif("HEIC/image.heic", convert_hdr_to_8bit=False)
+#     print("image size:", heif_file.size)
+#     print("image mode:", heif_file.mode)
+#     print("image data length:", len(heif_file.data))
+#     print("image data stride:", heif_file.stride)
+
+
+if pillow_heif.is_supported("HEIC/image.heic"):
+    heif_file = pillow_heif.open_heif("HEIC/image.heic")
+    np_array = np.asarray(heif_file)
--- HEIC/verify_HEIC.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image, ImageFilter
-from pillow_heif import HeifImagePlugin
-
-image = Image.open(Path("test.heic"))
-image.save("output.heic", exif=None, xmp=None)
