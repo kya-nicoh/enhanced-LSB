c250e0d2de3e418e240eabd8eb715380395882db
Nicoh <40926666+kya-nicoh@users.noreply.github.com>
2024-01-29 11:17:04 +0800

    lorenz generate num and non-repeating

--- lorenz attractor/lorenz-mod.py
+++ lorenz attractor/lorenz-mod.py
@@ -11,6 +11,7 @@ def lorenz(t, xyz, sigma=10, rho=28, beta=8/3):
 
 # Solve the Lorenz system
 initial_conditions = [1.0, 1.0, 1.0]
+
 t_span = (0, 100)
 t_eval = np.linspace(*t_span, num=10000)
 sol = solve_ivp(lorenz, t_span, initial_conditions, t_eval=t_eval)
@@ -21,7 +22,27 @@ np.random.seed(int(random_seeds[0]))
 
 # Generate random integers within a specified range
 lower_bound = 0
-upper_bound = 100
-num_random_numbers = 10
+upper_bound = 509
+
+# 1 letter = 6 numbers/ 3 pixels
+sample_letters = "h"
+
+num_random_numbers = len(sample_letters) * 6
 random_numbers = np.random.randint(lower_bound, upper_bound, size=num_random_numbers)
-print("Random numbers within the range:", random_numbers)
+
+paired_numbers = random_numbers.reshape(-1,2)
+random_number_pairs = [tuple(row) for row in paired_numbers]
+print(random_number_pairs)
+
+def duplicates_test(random_number_pairs):
+    counts = {}
+    # Count occurrences of each tuple in the list
+    for pair in random_number_pairs:
+        if pair in counts:
+            counts[pair] += 1
+        else:
+            counts[pair] = 1
+
+    # Filter tuples with counts greater than 1 (i.e., duplicates)
+    duplicates = {key: value for key, value in counts.items() if value > 1}
+    print("Duplicates:", duplicates)
